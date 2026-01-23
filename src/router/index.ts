d25d77fb51d3550b30189aff1528b64eff6adfd4
David Leonardo Molina Ruíz <dmolina101@gmail.com>
2026-01-23 12:50:15 -0400

    update

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/home/Index.vue';
-const ActivateUserAccount = () => import('../views/signup/ActivateUserAccount.vue');
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,11 +8,6 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: Home
-    },
-    {
-      path: "/activate-user-account",
-      name: "activate-user-account",
-      component: () => import('../views/signup/ActivateUserAccount.vue')
     },
     {
       path: "/event/:url",
